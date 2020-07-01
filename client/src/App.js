b009790d602cd833c4192806a821ad2819fc3a61
yaqootturman <yaqoot.turman@gmail.com>
2020-07-01 09:20:27 +0300

    hello world

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HELLO, WORLD!
         </p>
         <a
           className="App-link"
@@ -15,7 +14,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
