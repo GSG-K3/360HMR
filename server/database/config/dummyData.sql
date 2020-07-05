18582f89223b48f038e6488c50a001c0681d5f72
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-07-05 15:47:27 +0300

    insert data into tables

--- server/database/config/dummyData.sql
+++ server/database/config/dummyData.sql
@@ -1,116 +1,273 @@
-BEGIN;
 
-  INSERT INTO employees
-    (id,name ,email,image,date,position,role)
-  VALUES
-    ('1', 'yaqoot', '', '', 'programming', 'employee'),
-    ('2', 'haneen', '', '', 'programming', 'manager'),
-    ('3', 'hussein', '', '', 'programming', 'employee'),
-    ('4', 'karmel', '', '', 'programming', 'employee'),
-    ('5', 'ruba', '', '', 'programming', 'employee'),
-    ('6', 'ishak', '', '', 'marketing', 'manager');
+INSERT INTO employees
+  (name ,email,image,date,position,role)
+VALUES
+  ('haneen', '', '', '2018-04-01', 'programming', 'manager'),
+  ('ishak', '', '', '2018-08-01', 'marketing', 'manager'),
+  ('yaqoot', '', '', '2019-12-01', 'programming', 'employee'),
+  ('hussein', '', '', '2020-04-01', 'programming', 'employee'),
+  ('karmel', '', '', '2020-08-01', 'programming', 'employee'),
+  ('ruba', '', '', '2021-12-01', 'programming', 'employee');
 
-  INSERT INTO questions
-    (id,employee_id,type,result_rate,context)
-  VALUES
-    ('1', '1', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '1', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '1', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '1', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '1', 'write', '', 'Other notes about this employee?'),
-    ('6', '1', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '1', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '1', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '1', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '1', 'rate', '3', 'How satisfied are you with this employee handling of company customers?'),
+INSERT INTO questions
+  (type,result_rate,answer,context)
+VALUES
+  ( 'write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ( 'write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?'),
 
-    ('1', '2', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '2', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '2', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '2', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '2', 'write', '', 'Other notes about this employee?'),
-    ('6', '2', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '2', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '2', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '2', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '2', 'rate', '3', 'How satisfied are you with this employee handling of company customers?'),
+  ('write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ('write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?'),
 
-    ('1', '3', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '3', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '3', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '3', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '3', 'write', '', 'Other notes about this employee?'),
-    ('6', '3', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '3', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '3', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '3', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '3', 'rate', '3', 'How satisfied are you with this employee handling of company customers?'),
+  ('write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ('write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?'),
 
-    ('1', '4', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '4', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '4', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '4', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '4', 'write', '', 'Other notes about this employee?'),
-    ('6', '4', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '4', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '4', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '4', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '4', 'rate', '3', 'How satisfied are you with this employee handling of company customers?'),
+  ('write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ('write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?'),
 
-    ('1', '5', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '5', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '5', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '5', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '5', 'write', '', 'Other notes about this employee?'),
-    ('6', '5', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '5', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '5', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '5', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '5', 'rate', '3', 'How satisfied are you with this employee handling of company customers?'),
+  ('write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ('write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?'),
 
-    ('1', '6', 'write', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
-    ('2', '6', 'write', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
-    ('3', '6', 'write', '', 'Do you think that this employee is interested in time management? And why?'),
-    ('4', '6', 'write', '', 'Describe to me the method that this employee used to solve a specific problem?'),
-    ('5', '6', 'write', '', 'Other notes about this employee?'),
-    ('6', '6', 'rate', '4', 'How satisfied are you with this employee in terms of cooperation at work?'),
-    ('7', '6', 'rate', '1', 'Do you think this employee is in the right place in this company?'),
-    ('8', '6', 'rate', '5', 'How committed is this employee to the attendance estimate?'),
-    ('9', '6', 'rate', '2', 'How satisfied are you with this employee ability to withstand work pressure?'),
-    ('10', '6', 'rate', '3', 'How satisfied are you with this employee handling of company customers?');
+  ('write', '', '', 'What are the pros and cons you see in this employee dealings with other employees?'),
+  ('write', '', '', 'By collaborating with this employee, describe how his dealt with a problem that occurred?'),
+  ('write', '', '', 'Do you think that this employee is interested in time management? And why?'),
+  ('write', '', '', 'Describe to me the method that this employee used to solve a specific problem?'),
+  ('write', '', '', 'Other notes about this employee?'),
+  ('rate', '4', '', 'How satisfied are you with this employee in terms of cooperation at work?'),
+  ('rate', '1', '', 'Do you think this employee is in the right place in this company?'),
+  ('rate', '5', '', 'How committed is this employee to the attendance estimate?'),
+  ('rate', '2', '', 'How satisfied are you with this employee ability to withstand work pressure?'),
+  ('rate', '3', '', 'How satisfied are you with this employee handling of company customers?');
 
-  INSERT INTO employees_questions
-    (question_id,employee_id )
-  VALUES
-    ('', '');
+INSERT INTO employees_questions
+  (question_id,employee_id )
+VALUES
+  ('1', '1'),
+  ('2', '1'),
+  ('3', '1'),
+  ('4', '1'),
+  ('5', '1'),
+  ('6', '1'),
+  ('7', '1'),
+  ('8', '1'),
+  ('9', '1'),
+  ('10', '1'),
 
-  CREATE TABLE forms
-  (
-    id SERIAL PRIMARY KEY,
-    time date,
-    employee_id INT,
-    FOREIGN KEY(employee_id) REFERENCES employees(id)
-  );
-
-
-
-  CREATE TABLE employee_form
-  (
-    employee_id INT,
-    FOREIGN KEY (employee_id)  REFERENCES employees(id),
-
-    form_id INT,
-    FOREIGN KEY (form_id)  REFERENCES forms(id)
-  );
-
-
-  CREATE TABLE form_questions
-  (
-    form_id INT,
-    question_id INT,
-    FOREIGN KEY (form_id) REFERENCES forms(id),
-    FOREIGN KEY(question_id) REFERENCES questions(id)
-  );
+  ('1', '2'),
+  ('2', '2'),
+  ('3', '2'),
+  ('4', '2'),
+  ('5', '2'),
+  ('6', '2'),
+  ('7', '2'),
+  ('8', '2'),
+  ('9', '2'),
+  ('10', '2'),
 
 
-  COMMIT;
+  ('1', '3'),
+  ('2', '3'),
+  ('3', '3'),
+  ('4', '3'),
+  ('5', '3'),
+  ('6', '3'),
+  ('7', '3'),
+  ('8', '3'),
+  ('9', '3'),
+  ('10', '3'),
+
+  ('1', '4'),
+  ('2', '4'),
+  ('3', '4'),
+  ('4', '4'),
+  ('5', '4'),
+  ('6', '4'),
+  ('7', '4'),
+  ('8', '4'),
+  ('9', '4'),
+  ('10', '4'),
+
+  ('1', '5'),
+  ('2', '5'),
+  ('3', '5'),
+  ('4', '5'),
+  ('5', '5'),
+  ('6', '5'),
+  ('7', '5'),
+  ('8', '5'),
+  ('9', '5'),
+  ('10', '5'),
+
+  ('1', '6'),
+  ('2', '6'),
+  ('3', '6'),
+  ('4', '6'),
+  ('5', '6'),
+  ('6', '6'),
+  ('7', '6'),
+  ('8', '6'),
+  ('9', '6'),
+  ('10', '6');
+
+
+
+INSERT INTO employee_form
+  (employee_id ,form_id,time )
+VALUES
+  ('1', '1', '2018-05-05'),
+  ('1', '2', '2018-06-05'),
+  ('1', '3', '2018-07-05'),
+  ('1', '4', '2018-08-05'),
+  ('1', '5', '2018-09-05'),
+  ('1', '6', '2018-10-05'),
+
+  ('2', '1', '2018-08-05'),
+  ('2', '2', '2018-09-05'),
+  ('2', '3', '2018-10-05'),
+  ('2', '4', '2018-11-05'),
+  ('2', '5', '2018-12-05'),
+  ('2', '6', '2019-01-05'),
+
+  ('3', '1', '2019-12-01'),
+  ('3', '2', '2019-12-07'),
+  ('3', '3', '2019-12-13'),
+  ('3', '4', '2019-12-19'),
+  ('3', '5', '2019-12-25'),
+  ('3', '6', '2020-12-31'),
+
+  ('4', '1', '2020-04-01'),
+  ('4', '2', '2020-04-07'),
+  ('4', '3', '2020-04-13'),
+  ('4', '4', '2020-04-19'),
+  ('4', '5', '2020-04-25'),
+  ('4', '6', '2020-04-30'),
+
+  ('5', '1', '2020-08-01'),
+  ('5', '2', '2020-08-07'),
+  ('5', '3', '2020-08-13'),
+  ('5', '4', '2020-08-19'),
+  ('5', '5', '2020-08-25'),
+  ('5', '6', '2020-08-31'),
+
+  ('6', '1', '2021-12-01'),
+  ('6', '2', '2021-12-07'),
+  ('6', '3', '2021-12-13'),
+  ('6', '4', '2021-12-19'),
+  ('6', '5', '2021-12-25'),
+  ('6', '6', '2021-12-31');
+
+
+
+INSERT INTO form_questions
+  (form_id ,question_id)
+VALUES
+  ('1', '1'),
+  ('2', '1'),
+  ('3', '1'),
+  ('4', '1'),
+  ('5', '1'),
+  ('6', '1'),
+
+  ('1', '2'),
+  ('2', '2'),
+  ('3', '2'),
+  ('4', '2'),
+  ('5', '2'),
+  ('6', '2'),
+
+  ('1', '3'),
+  ('2', '3'),
+  ('3', '3'),
+  ('4', '3'),
+  ('5', '3'),
+  ('6', '3'),
+
+
+  ('1', '4'),
+  ('2', '4'),
+  ('3', '4'),
+  ('4', '4'),
+  ('5', '4'),
+  ('6', '4'),
+
+  ('1', '5'),
+  ('2', '5'),
+  ('3', '5'),
+  ('4', '5'),
+  ('5', '5'),
+  ('6', '5'),
+
+  ('1', '6'),
+  ('2', '6'),
+  ('3', '6'),
+  ('4', '6'),
+  ('5', '6'),
+  ('6', '6'),
+
+  ('1', '7'),
+  ('2', '7'),
+  ('3', '7'),
+  ('4', '7'),
+  ('5', '7'),
+  ('6', '7'),
+
+  ('1', '8'),
+  ('2', '8'),
+  ('3', '8'),
+  ('4', '8'),
+  ('5', '8'),
+  ('6', '8'),
+
+  ('1', '9'),
+  ('2', '9'),
+  ('3', '9'),
+  ('4', '9'),
+  ('5', '9'),
+  ('6', '9'),
+
+  ('1', '10'),
+  ('2', '10'),
+  ('3', '10'),
+  ('4', '10'),
+  ('5', '10'),
+  ('6', '10');
+
