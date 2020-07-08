9bd3656050179f1061d420f87a21fcf783a2bed1
karmelyoei <karmel.salah@gmail.com>
2020-07-08 12:00:55 +0300

    adding the router post method for the form endpoint to adding the data into db #37

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./controller');
+const { join } = require('path');
 
 const app = express();
 
@@ -7,5 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 module.exports = app;
