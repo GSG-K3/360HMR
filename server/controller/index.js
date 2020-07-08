5802f74605ed9201b862bf26452093a191d8a383
karmelyoei <karmel.salah@gmail.com>
2020-07-08 12:01:35 +0300

    functions for adding data into db #37

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
-
+const insertQuestions = require('./insertQuestions');
+router.post('/api/dashboard/form', insertQuestions);
 module.exports = router;
--- server/database/config/db_connection.js
+++ server/database/config/db_connection.js
@@ -4,9 +4,9 @@ require('env2')('./config.env');
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
-  throw new Error('Cannot reach to database check your code please!');
+	throw new Error('Cannot reach to database check your code please!');
 }
 module.exports = new Pool({
-  connectionString: connectionString,
-  ssl: true,
+	connectionString: connectionString,
+	ssl: { rejectUnauthorized: false },
 });
