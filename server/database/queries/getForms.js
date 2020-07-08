e45b1ad41ce4dc175d616a2b8f830170f1d03b4d
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-07-08 16:03:04 +0300

    change the query by removing result_rate from result of query

--- server/database/queries/getForms.js
+++ server/database/queries/getForms.js
@@ -3,7 +3,7 @@ const db_connection = require('../config/db_connection');
 const getForms = (employeeID) => {
 	const sql = {
 		text:
-			' SELECT e.name employee_form,f.reviewer_name ,q.context question,eq.result_rate rate_answer,eq.answer context_answer,q.role FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1;',
+			' SELECT e.name employee_form,f.reviewer_name ,q.context question,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1;',
 		values: [employeeID],
 	};
 
