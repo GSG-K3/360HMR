fac9b1d082c20672eae2a3b68448c40f96b33800
karmelyoei <karmel.salah@gmail.com>
2020-07-13 15:31:33 +0300

    adding the colors #49

--- client/src/theme/MaterialUiTheme.js
+++ client/src/theme/MaterialUiTheme.js
@@ -2,7 +2,12 @@ import { createMuiTheme } from '@material-ui/core';
 const Theme = createMuiTheme({
 	direction: 'rtl',
 	palette: {
-		primary: { main: '#4CB7A7' },
+		primary: { main: '#0D005B' },
+		secondary: { main: '#160196' },
+	},
+	colors: {
+		purpuleLight: '#3C327B',
+		gray: '#E4DDDD',
 	},
 });
 export default Theme;
