535a5e1c6977dfb48a2b4513e29d36a1329e634b
rubasider <rubashka12@gmail.com>
2020-07-14 12:43:04 +0300

    change colors

--- client/src/theme/MaterialUiTheme.js
+++ client/src/theme/MaterialUiTheme.js
@@ -2,8 +2,8 @@ import { createMuiTheme } from '@material-ui/core';
 const Theme = createMuiTheme({
 	direction: 'rtl',
 	palette: {
-		primary: { main: '#0D005B' },
-		secondary: { main: '#160196' },
+		primary: { main: '#11205A' },
+		secondary: { main: '#DBD9E3' },
 	},
 	colors: {
 		purpuleLight: '#3C327B',
