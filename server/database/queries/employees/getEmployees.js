e26e5f22b202d259a873e3c866360e9195003d9c
rubasider <rubashka12@gmail.com>
2020-07-16 16:44:33 +0300

    d

--- server/database/queries/employees/getEmployees.js
+++ server/database/queries/employees/getEmployees.js
@@ -5,5 +5,5 @@ module.exports = () => {
 		text:
 			'select DISTINCT name,id,image from employees INNER JOIN forms ON forms.reviewer_name = employees.name INNER JOIN employee_form ON forms.id =employee_form.form_id WHERE forms.reviewer_name IS NOT NULL;',
 	};
-	return connection.query(sql.text, sql.value);
+	return connection.query(sql.text);
 };
