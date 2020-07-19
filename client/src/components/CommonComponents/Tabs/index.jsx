e8b6c3adbb02f8a91f7fa3b538cf9705ae752fcd
karmelyoei <karmel.salah@gmail.com>
2020-07-19 09:45:34 +0300

    changing the varliables names #54

--- client/src/components/CommonComponents/Tabs/index.jsx
+++ client/src/components/CommonComponents/Tabs/index.jsx
@@ -3,7 +3,7 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import useStyles from './style';
 
-export default function DisabledTabs() {
+export default function Tabs() {
 	const [value, setValue] = React.useState(0);
 
 	const handleChange = (event, newValue) => {
