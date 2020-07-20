24c3e497b54e840fbc31a206ed46e785a06516e7
rubasider <rubashka12@gmail.com>
2020-07-20 21:58:46 +0300

    the query

--- server/database/queries/employees/getEmpById.js
+++ server/database/queries/employees/getEmpById.js
@@ -2,10 +2,9 @@ const db_connection = require('../../config/db_connection');
 
 const getForms = (employeeID) => {
 	const sql = {
-		// text: ' SELECT name from employees where id=$1;',
 		text:
-			// ' SELECT   e.name employee_form,f.reviewer_name ,q.context question,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1;',
-			'SELECT e.name employee_form,f.reviewer_name ,q.context question,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1;',
+			'select DISTINCT e.id employee_id,f.reviewer_name from employees e, forms f , employee_form ef where e.id=$1 AND ef.form_id=f.id;',
+
 		values: [employeeID],
 	};
 
