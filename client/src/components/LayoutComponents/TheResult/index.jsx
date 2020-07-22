a9d43459ddc99eec0104c10b90f26802b906dde2
rubasider <rubashka12@gmail.com>
2020-07-22 11:46:52 +0300

    the result page and i used stars to show the employee rating

--- client/src/components/LayoutComponents/TheResult/index.jsx
+++ client/src/components/LayoutComponents/TheResult/index.jsx
@@ -1,47 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import {
-	Button,
-	Box,
-	List,
-	ListItem,
-	Divider,
-	ListItemText,
-	ListItemAvatar,
-	Grid,
-	Typography,
-} from '@material-ui/core';
-import { Redirect, Link } from 'react-router-dom';
+import { Box, Divider, Grid, Typography } from '@material-ui/core';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Rating from '@material-ui/lab/Rating';
 import { useParams } from 'react-router';
-import { makeStyles } from '@material-ui/core/styles';
+
 import swal from 'sweetalert';
 import axios from 'axios';
 import LoaderProgress from '../../CommonComponents/LoaderProgress';
-import { Person } from '@material-ui/icons';
-// import ResultReviewStyle from './ResultReviewStyle';
+
+import theResult from './theResult';
 
 export default function ResultReview() {
 	const [result, setResult] = useState(null);
 	const [isLoading, setIsLoading] = useState(true);
 	const params = useParams();
+	const classes = theResult();
 
-	// const theData = { ...data.location.state };
-	// setResult(TheData);
 	useEffect(() => {
 		axios
 			.get(`/api/dashboard/history/${params.id}/response/${params.name}`)
 			.then((result) => {
 				const emp = { ...result.data.data };
-				console.log(emp);
+
 				setResult(emp);
 			})
 			.catch((err) => {
 				if (err.response.data) swal('Error', err.response.data.messag, 'error');
 			});
 	}, []);
-	const handleClick = (id) => {
-		// console.log('id', id);
-		// return <Link to={`/dashboard/history/${id}`} />;
-	};
+
 	const buildAnswers = (results) => {
 		const empNames = [];
 		if (!results) {
@@ -51,15 +38,37 @@ export default function ResultReview() {
 		const keyLength = Object.keys(results).length;
 		for (let emp of Object.keys(results)) {
 			const empValue = results[emp];
-
-			empNames.push(
-				<React.Fragment>
-					<Typography variant="h5">Question{count++} </Typography>
-					<Typography>{empValue.question}</Typography>
-					<Typography>{empValue.context_answer}</Typography>
-					<Divider variant="middle" />
-				</React.Fragment>,
-			);
+			if (empValue.type === 'نصي')
+				empNames.push(
+					<React.Fragment>
+						<Grid className={classes.texts}>
+							<Typography variant="h5">السؤال {count++}</Typography>
+							<Typography>{empValue.question}</Typography>
+							<Typography>{empValue.context_answer}</Typography>
+						</Grid>
+						<Divider variant="middle" />
+					</React.Fragment>,
+				);
+			if (empValue.type === 'تقيمي') {
+				empNames.push(
+					<React.Fragment>
+						<Grid className={classes.texts}>
+							<Box component="fieldset" mb={3} borderColor="transparent">
+								<Typography variant="h5"> السؤال {count++}</Typography>
+								<Typography>{empValue.question}</Typography>
+								{/* <Typography component="legend">Rating</Typography> */}
+								<Rating
+									name="customized-empty"
+									defaultValue={empValue.context_answer}
+									precision={0.5}
+									emptyIcon={<StarBorderIcon fontSize="inherit" />}
+								/>
+							</Box>
+						</Grid>
+						<Divider variant="middle" />
+					</React.Fragment>,
+				);
+			}
 		}
 
 		if (empNames.length === keyLength && isLoading) {
--- server/database/queries/forms/getResultForm.js
+++ server/database/queries/forms/getResultForm.js
@@ -3,7 +3,7 @@ const db_connection = require('../../config/db_connection');
 const getResultForm = (employeeID, reviewer_name) => {
 	const sql = {
 		text:
-			' SELECT e.name employee_form,f.reviewer_name ,q.context question,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1 AND f.reviewer_name=$2;',
+			' SELECT e.name employee_form,f.reviewer_name ,q.context question,q.type,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1 AND f.reviewer_name=$2;',
 
 		values: [employeeID, reviewer_name],
 	};
