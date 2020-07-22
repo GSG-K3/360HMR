b54b7b67fc57e2ff11ff0e22e355b9613832ff54
yaqootturman <yaqoot.turman@gmail.com>
2020-07-22 18:01:18 +0300

    backend corrections

--- server/database/queries/forms/getResultForm.js
+++ server/database/queries/forms/getResultForm.js
@@ -1,13 +1,8 @@
 const db_connection = require('../../config/db_connection');
 
-const getResultForm = (employeeID, reviewer_name) => {
-	const sql = {
-		text:
-			' SELECT e.name employee_form,f.reviewer_name ,q.context question,q.type,eq.answer context_answer FROM questions q, employees e,employees_questions eq,form_questions fq,forms f WHERE e.id=eq.employee_id AND q.id=eq.question_id AND fq.form_id=f.id AND fq.question_id=q.id AND eq.employee_id=$1 AND f.reviewer_name=$2;',
+exports.getResult = (employeeID, name) => {
 
-		values: [employeeID, reviewer_name],
-	};
+	return db_connection.query('select * from (select * from employees_questions WHERE employee_id =$1 AND reviewer_name=$2) as RQ INNER JOIN questions ON questions.id= RQ.question_id', [employeeID, name]);
 
-	return db_connection.query(sql);
-};
-module.exports = getResultForm;
+}
+
